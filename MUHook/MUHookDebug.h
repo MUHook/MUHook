0cade30e5ce70b11431acfc447370e023b28d95c
冷秋 <516563564@qq.com>
2019-01-23 11:17:09 +0800

    Opt: Macros

--- MUHook/MUHookDebug.h
+++ MUHook/MUHookDebug.h
@@ -16,7 +16,8 @@
 
 #pragma mark - Quick Statement
 
-#define MUHClass(c)                             ((Class)(((c *)0x0), objc_getClass(#c)))
+#define _MUHClass(c)                            ((Class)(((c *)0x0), objc_getClass(#c)))
+#define MUHClass(c)                             _MUHClass(c)
 
 #define MUHSendClassMsg(c, factory)             [MUHClass(c) factory]
 
@@ -28,51 +29,61 @@
 
 #pragma mark - Implementation
 
-#define MUHInstanceImplementation(c, name, returnType, args...) \
+#define _MUHInstanceImplementation(c, name, returnType, args...) \
 typedef void *I_##c##_##name;   \
 __unused static I_##c##_##name name;     \
 __unused static returnType (*_unique_objc_ori$##c##$##name)   ( c * self, SEL _cmd, ##args ); \
 __unused static returnType   _unique_objc_new$##c##$##name    ( c * self, SEL _cmd, ##args )
+#define MUHInstanceImplementation(c, name, returnType, args...) _MUHInstanceImplementation(c, name, returnType, ##args)
 
-#define MUHClassImplementation(c, name, returnType, args...) \
+#define _MUHClassImplementation(c, name, returnType, args...) \
 typedef void *C_##c##_##name;   \
 __unused static C_##c##_##name name;     \
 __unused static returnType (*_unique_objc_ori$##c##$##name)   ( __strong const Class self, SEL _cmd, ##args ); \
 __unused static returnType   _unique_objc_new$##c##$##name    ( __strong const Class self, SEL _cmd, ##args )
+#define MUHClassImplementation(c, name, returnType, args...) _MUHClassImplementation(c, name, returnType, ##args)
 
-#define MUHSymbolImplementation(symbol, returnType, args...) \
+#define _MUHSymbolImplementation(symbol, returnType, args...) \
 _Pragma("clang diagnostic push") \
 _Pragma("clang diagnostic ignored \"-Wstrict-prototypes\"") \
 __unused static returnType (*_unique_symbol_ori$##symbol)(args); \
 __unused static returnType   _unique_symbol_new$##symbol (args) \
 _Pragma("clang diagnostic pop")
+#define MUHSymbolImplementation(symbol, returnType, args...) _MUHSymbolImplementation(symbol, returnType, ##args)
 
 #pragma mark - Execute Orig or Super
 
-#define MUHOrig(c, name, args...)               (((c *)0x0),(void)name,(!_unique_objc_ori$##c##$##name?0:_unique_objc_ori$##c##$##name (self, _cmd, ##args)))
-#define MUHSuper(c, name, args...)              (((c *)0x0),(void)name,(!_unique_objc_ori$##c##$##name?0:_unique_objc_ori$##c##$##name (self, _cmd, ##args)))
+#define _MUHOrig(c, name, args...)              (((c *)0x0),(void)name,(!_unique_objc_ori$##c##$##name?0:_unique_objc_ori$##c##$##name (self, _cmd, ##args)))
+#define MUHOrig(c, name, args...)               _MUHOrig(c, name, ##args)
 
-#define MUHSymbolOrig(symbol, args...)          ((void)&symbol,_unique_symbol_ori$##symbol(args))
+#define _MUHSuper(c, name, args...)             (((c *)0x0),(void)name,(!_unique_objc_ori$##c##$##name?0:_unique_objc_ori$##c##$##name (self, _cmd, ##args)))
+#define MUHSuper(c, name, args...)              _MUHSuper(c, name, ##args)
+
+#define _MUHSymbolOrig(symbol, args...)         ((void)&symbol,_unique_symbol_ori$##symbol(args))
+#define MUHSymbolOrig(symbol, args...)         _MUHSymbolOrig(symbol, ##args)
 
 #pragma mark - Hook
 
-#define MUHHookInstanceMessage(c, name, sel) \
+#define _MUHHookInstanceMessage(c, name, sel) \
 {(c *)0x0;(void)name;}MUHookInstanceMessageEx(objc_getClass( #c ), @selector(sel), (IMP)&_unique_objc_new$##c##$##name, (IMP*)&_unique_objc_ori$##c##$##name);
+#define MUHHookInstanceMessage(c, name, sel) _MUHHookInstanceMessage(c, name, sel)
 
-#define MUHHookClassMessage(c, name, sel) \
+#define _MUHHookClassMessage(c, name, sel) \
 {(c *)0x0;(void)name;}MUHookClassMessageEx(objc_getClass( #c ), @selector(sel), (IMP)&_unique_objc_new$##c##$##name, (IMP*)&_unique_objc_ori$##c##$##name);
+#define MUHHookClassMessage(c, name, sel) _MUHHookClassMessage(c, name, sel)
 
-#define MUHHookSymbolFunction(symbol) \
+#define _MUHHookSymbolFunction(symbol) \
 {(void)&symbol;}MURebindSymbol(#symbol, _unique_symbol_new$##symbol, (void *)&_unique_symbol_ori$##symbol)
+#define MUHHookSymbolFunction(symbol) _MUHHookSymbolFunction(symbol)
 
 #pragma mark - Create
 
-//#define MUHCreateClass(c, sc) {(c *)0x0;(sc *)0x0;}MUCreateClass(#c, #sc)
-
-#define MUHAddInstanceMethod(c, name, sel, encode) \
+#define _MUHAddInstanceMethod(c, name, sel, encode) \
 {(c *)0x0;(void)name;}MUAddInstanceMessageEx(objc_getClass( #c ), @selector(sel), (IMP)&_unique_objc_new$##c##$##name, #encode , (IMP*)&_unique_objc_ori$##c##$##name)
+#define MUHAddInstanceMethod(c, name, sel, encode) _MUHAddInstanceMethod(c, name, sel, encode)
 
-#define MUHAddClassMethod(c, name, sel, encode) \
+#define _MUHAddClassMethod(c, name, sel, encode) \
 {(c *)0x0;(void)name;}MUAddClassMessageEx(objc_getMetaClass( #c ), @selector(sel), (IMP)&_unique_objc_new$##c##$##name, #encode , (IMP*)&_unique_objc_ori$##c##$##name)
+#define MUHAddClassMethod(c, name, sel, encode) _MUHAddClassMethod(c, name, sel, encode)
 
 #endif
--- MUHook/MUHookRelease.h
+++ MUHook/MUHookRelease.h
@@ -17,7 +17,8 @@
 
 #pragma mark - Quick Statement
 
-#define MUHClass(c)                             objc_getClass(#c)
+#define _MUHClass(c)                            objc_getClass(#c)
+#define MUHClass(c)                             _MUHClass(c)
 
 #define MUHSendClassMsg(c, factory)             [MUHClass(c) factory]
 
@@ -29,45 +30,57 @@
 
 #pragma mark - Implementation
 
-#define MUHInstanceImplementation(c, name, returnType, args...) \
+#define _MUHInstanceImplementation(c, name, returnType, args...) \
 __unused static returnType (*_unique_objc_ori$##c##$##name)   ( c * self, SEL _cmd, ##args ); \
 __unused static returnType   _unique_objc_new$##c##$##name    ( c * self, SEL _cmd, ##args )
+#define MUHInstanceImplementation(c, name, returnType, args...) _MUHInstanceImplementation(c, name, returnType, ##args)
 
-#define MUHClassImplementation(c, name, returnType, args...) \
+#define _MUHClassImplementation(c, name, returnType, args...) \
 __unused static returnType (*_unique_objc_ori$##c##$##name)   ( __strong const Class self, SEL _cmd, ##args ); \
 __unused static returnType   _unique_objc_new$##c##$##name    ( __strong const Class self, SEL _cmd, ##args )
+#define MUHClassImplementation(c, name, returnType, args...) _MUHClassImplementation(c, name, returnType, ##args)
 
-#define MUHSymbolImplementation(symbol, returnType, args...) \
+#define _MUHSymbolImplementation(symbol, returnType, args...) \
 _Pragma("clang diagnostic push") \
 _Pragma("clang diagnostic ignored \"-Wstrict-prototypes\"") \
 __unused static returnType (*_unique_symbol_ori$##symbol)(args); \
 __unused static returnType   _unique_symbol_new$##symbol (args)  \
 _Pragma("clang diagnostic pop")
+#define MUHSymbolImplementation(symbol, returnType, args...) _MUHSymbolImplementation(symbol, returnType, ##args)
 
 #pragma mark - Execute Orig or Super
 
-#define MUHOrig(c, name, args...)               (!_unique_objc_ori$##c##$##name?0:_unique_objc_ori$##c##$##name (self, _cmd, ##args))
-#define MUHSuper(c, name, args...)              (!_unique_objc_ori$##c##$##name?0:_unique_objc_ori$##c##$##name (self, _cmd, ##args))
+#define _MUHOrig(c, name, args...)          (!_unique_objc_ori$##c##$##name?0:_unique_objc_ori$##c##$##name (self, _cmd, ##args))
+#define MUHOrig(c, name, args...)           _MUHOrig(c, name, ##args)
 
-#define MUHSymbolOrig(symbol, args...)          (_unique_symbol_ori$##symbol(args))
+#define _MUHSuper(c, name, args...)         (!_unique_objc_ori$##c##$##name?0:_unique_objc_ori$##c##$##name (self, _cmd, ##args))
+#define MUHSuper(c, name, args...)          _MUHSuper(c, name, ##args)
+
+#define _MUHSymbolOrig(symbol, args...)     (_unique_symbol_ori$##symbol(args))
+#define MUHSymbolOrig(symbol, args...)      _MUHSymbolOrig(symbol, ##args)
 
 #pragma mark - Hook
 
-#define MUHHookInstanceMessage(c, name, sel) \
+#define _MUHHookInstanceMessage(c, name, sel) \
 MUHookInstanceMessageEx(objc_getClass( #c ), @selector(sel), (IMP)&_unique_objc_new$##c##$##name, (IMP*)&_unique_objc_ori$##c##$##name)
+#define MUHHookInstanceMessage(c, name, sel) _MUHHookInstanceMessage(c, name, sel)
 
-#define MUHHookClassMessage(c, name, sel) \
+#define _MUHHookClassMessage(c, name, sel) \
 MUHookClassMessageEx(objc_getClass( #c ), @selector(sel), (IMP)&_unique_objc_new$##c##$##name, (IMP*)&_unique_objc_ori$##c##$##name)
+#define MUHHookClassMessage(c, name, sel) _MUHHookClassMessage(c, name, sel)
 
-#define MUHHookSymbolFunction(symbol) \
+#define _MUHHookSymbolFunction(symbol) \
 MURebindSymbol(#symbol, _unique_symbol_new$##symbol, (void *)&_unique_symbol_ori$##symbol)
+#define MUHHookSymbolFunction(symbol) _MUHHookSymbolFunction(symbol)
 
 #pragma mark - Create
 
-#define MUHAddInstanceMethod(c, name, sel, encode) \
+#define _MUHAddInstanceMethod(c, name, sel, encode) \
 MUAddInstanceMessageEx(objc_getClass( #c ), @selector(sel), (IMP)&_unique_objc_new$##c##$##name, #encode , (IMP*)&_unique_objc_ori$##c##$##name)
+#define MUHAddInstanceMethod(c, name, sel, encode) _MUHAddInstanceMethod(c, name, sel, encode)
 
-#define MUHAddClassMethod(c, name, sel, encode) \
+#define _MUHAddClassMethod(c, name, sel, encode) \
 MUAddClassMessageEx(objc_getMetaClass( #c ), @selector(sel), (IMP)&_unique_objc_new$##c##$##name, #encode , (IMP*)&_unique_objc_ori$##c##$##name)
+#define MUHAddClassMethod(c, name, sel, encode) _MUHAddClassMethod(c, name, sel, encode)
 
 #endif
